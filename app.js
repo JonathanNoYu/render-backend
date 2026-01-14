29f2fb3f78f8cfa555ee5c72588f22cc167ce62f
JonathanNoYu <jonathanyu10@gmail.com>
2026-01-13 22:47:19 -0500

    Removed more unneeded code & comments

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 import express from "express";
-import cors from "cors";
-import session from "express-session";
 import "dotenv/config";
 import webScarpeRoutes from "./webScrape/routes.js";
 
 const app = express();
 
 const allowedOrigins = process.env.FRONTEND_URL
-console.log(allowedOrigins)
-// const sessionOptions = {
-//     secret: "any string",
-//     resave: false,
-//     saveUninitialized: false,
-// };
-// if (process.env.NODE_ENV !== "development") {
-//     sessionOptions.proxy = true;
-//     sessionOptions.cookie = {
-//         sameSite: "none",
-//         secure: true,
-//         store: new MemoryStore({
-//             checkPeriod: 360000 // prune expired entries every 10 mins
-//         }),
-//     };
-// }
-// app.use(session(sessionOptions));
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
